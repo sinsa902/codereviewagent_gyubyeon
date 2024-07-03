5a6affa237b4cb9dd7e423f8480b9463b91f9e66
gyu.byeon <isoul902@gmail.com>
2024-07-03 16:57:29 +0900

    3일차 update 2

--- 3day/ALU-main/ALU-main/python/alu.py
+++ 3day/ALU-main/ALU-main/python/alu.py
@@ -1,6 +1,7 @@
 from alu_result import ALU_result
 
-class ALU :
+
+class ALU:
     def __init__(self) -> None:
         super().__init__()
         self.operand1 = -1
@@ -16,40 +17,38 @@ class ALU :
     def set_opcode(self, opcode):
         self.opcode = opcode
 
-    def enable_signal(self, r : ALU_result):
-        if (self.opcode == "ADD") and (self.opcode != "MUL") and (self.opcode != "SUB") :
-            if self.operand1 != -1 and self.operand2 != -1 :
-                result = self.operand1 + self.operand2
-                r.set_result(result)
-                r.set_status(0)
-            elif self.operand1 == -1 :
-                r.set_result(65535)
-                r.set_status(1)
-            elif self.operand2 == -1 :
-                r.set_result(65535)
-                r.set_status(2)
-        elif (self.opcode != "ADD") and (self.opcode == "MUL") and (self.opcode != "SUB") :
-            if self.operand1 != -1 and self.operand2 != -1 :
-                result = self.operand1 * self.operand2
-                r.set_result(result)
-                r.set_status(0)
-            elif self.operand1 == -1 :
-                r.set_result(65535)
-                r.set_status(1)
-            elif self.operand2 == -1 :
-                r.set_result(65535)
-                r.set_status(2)
-        elif (self.opcode != "ADD") and (self.opcode != "MUL") and (self.opcode == "SUB"):
-            if self.operand1 != -1 and self.operand2 != -1 :
-                result = self.operand1 - self.operand2
-                r.set_result(result)
-                r.set_status(0)
-            elif self.operand1 == -1:
-                r.set_result(65535)
-                r.set_status(1)
-            elif self.operand2 == -1:
-                r.set_result(65535)
-                r.set_status(2)
-        else:
+    def enable_signal(self, r: ALU_result):
+        if not self.__is_valid1(r):
+            return
+        if not self.__is_valid2():
+            return
+
+        self.__get_operation(r)
+
+    def __get_operation(self, r):
+        result = {
+            "ADD": self.operand1 + self.operand2,
+            "MUL": self.operand1 * self.operand2,
+            "SUB": self.operand1 - self.operand2,
+        }.get(self.opcode, None)
+        r.set_result(result)
+        r.set_status(0)
+
+    def __is_valid1(self, r):
+        if not self.opcode in ["ADD", "MUL", "SUB"]:
             r.set_result(65535)
             r.set_status(3)
+            return False
+        return True
+
+    def __is_valid2(self):
+        if self.operand1 == -1:
+            r.set_result(65535)
+            r.set_status(1)
+            return False
+
+        if self.operand2 == -1:
+            r.set_result(65535)
+            r.set_status(2)
+            return False
+        return True
